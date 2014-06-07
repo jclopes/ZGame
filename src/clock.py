1ec082d6f6e8443b881e738980eef13196b9e981
João Lopes <jc.lopes@gmail.com>
2014-06-07 18:59:31 +0200

    add Clock class to keep track of elapsed time

---
+++ src/clock.py
@@ -0,0 +1,24 @@
+import time
+
+
+class Clock(object):
+    """Clock keeps track of the elapsed time and how much sleep is needed.
+
+    frametime -- is the time that should pass between calls.
+    """
+    def __init__(self, frametime):
+        super(Clock, self).__init__()
+        self.frametime = frametime
+        self.t0 = None
+
+    def start(self):
+        """Initialises the timer"""
+        self.t0 = time.time()
+
+    def sleep(self):
+        t1 = time.time()
+        td = t1 - self.t0
+        if td < self.frametime:
+            print "sleeping %s" % (self.frametime - td)
+            time.sleep(self.frametime - td)
+        self.t0 = t1
