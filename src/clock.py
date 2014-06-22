1ff22e9c4976f64ad3960016c6c53290e8e56c91
João Lopes <jc.lopes@gmail.com>
2014-06-22 14:51:35 +0200

    add ‘reset’ and ‘sleep_time’ to Clock

--- src/clock.py
+++ src/clock.py
@@ -4,7 +4,7 @@ import time
 class Clock(object):
     """Clock keeps track of the elapsed time and how much sleep is needed.
 
-    frametime -- is the time that should pass between calls.
+    frametime -- is the time (in seconds) that should pass between calls.
     """
     def __init__(self, frametime):
         super(Clock, self).__init__()
@@ -16,14 +16,18 @@ class Clock(object):
         self.t0 = time.time()
 
     def sleep(self):
+        st = self.sleep_time()
+        print "sleeping %s" % st
+        time.sleep(st)
+        self.t0 = time.time()
+
+    def sleep_time(self):
+        res = 0.0
         t1 = time.time()
         td = t1 - self.t0
         if td < self.frametime:
-            #print "sleeping %s" % (self.frametime - td)
-            time.sleep(self.frametime - td)
-        self.t0 = t1
+            res = (self.frametime - td)
+        return res
 
-    def left(self):
-        t1 = time.time()
-        td = t1 - self.t0
-        return self.frametime - td
+    def reset(self):
+        self.start()
